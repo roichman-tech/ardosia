bd28a73d3fb8bd6bc248b73ff4274c6c8d556c6e
Matheus Roichman <roichmankill@gmail.com>
2026-06-07 00:27:28 -0300

    feat: added ShadCN

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 import {
   ClerkProvider,
@@ -8,16 +8,9 @@ import {
   SignUpButton,
   UserButton,
 } from "@clerk/nextjs";
+import { cn } from "@/lib/utils";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -32,7 +25,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={cn("h-full", "antialiased", "font-sans", inter.variable)}
     >
       <body className="min-h-full flex flex-col">
         <ClerkProvider>
@@ -40,7 +33,10 @@ export default function RootLayout({
             <Show when="signed-out">
               <SignInButton />
               <SignUpButton>
-                <button type="button" className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+                <button
+                  type="button"
+                  className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer"
+                >
                   Sign Up
                 </button>
               </SignUpButton>
